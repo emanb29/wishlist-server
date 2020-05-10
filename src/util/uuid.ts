ad2c799b32971ff8e8ae91f07064e9f97007fced
emanb29 <eb@ethanbell.me>
2020-05-09 18:30:54 -0700

    We don't talk about v2

--- src/util/uuid.ts
+++ src/util/uuid.ts
@@ -1,6 +1,5 @@
 import * as uuid from 'uuid'
 export type uuid1 = string & { _opaquetype: 'uuidv1' }
-export type uuid2 = string & { _opaquetype: 'uuidv2' }
 export type uuid3 = string & { _opaquetype: 'uuidv3' }
 export type uuid4 = string & { _opaquetype: 'uuidv4' }
 export type uuid5 = string & { _opaquetype: 'uuidv5' }
